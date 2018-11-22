867d134bd349ea30b198b553c104f27c73e125c1
C00204864 <C00204864@itcarlow.ie>
2018-11-22 11:01:44 +0000

    Done on time

---
+++ AssertProject/AssertProject/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+bool checkNumberRange(int argc, int startPoint, char * argv[])
+{
+	for (int i = startPoint; i < argc; ++i)
+	{
+		for (int j = 0; j < strlen(argv[i]); j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				return false;
+			}
+		}
+		int num = atoi(argv[i]);
+		if (num < 1 || num > 46)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool checkRepetition(int argc, int startPoint, char * argv[])
+{
+	for (int i = startPoint; i < argc; ++i)
+	{
+		for (int j = i + 1; j < argc; ++j)
+		{
+			if (*argv[i] == *argv[j])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+int main(int argc, char * argv[])
+{
+	if (argc == 7 && checkNumberRange(argc, 1, argv) && checkRepetition(argc, 1, argv))
+	{
+		std::cout << "Valid Numbers" << std::endl;
+	}
+	else
+	{
+		std::cout << "Invalid Numbers" << std::endl;
+	}
+	return 0;
+}
---
+++ AssertProject/UnitTest/unittest.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\\AssertProject\main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{		
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestCheckNumberRange)
+		{
+			char * argv1[] = { "Anything", "2", "3", "4", "5", "6", "7" };
+			char * argv2[] = { "Anything", "111", "3", "4", "5", "6", "7" };
+			char * argv3[] = { "Anything", "-2", "3", "4", "5", "6", "7" };
+			char * argv4[] = { "Anything", "0", "3", "4", "5", "6", "789" };
+			Assert::AreEqual(true, checkNumberRange(7, 1, argv1));
+			Assert::AreEqual(false, checkNumberRange(7, 1, argv2));
+			Assert::AreEqual(false, checkNumberRange(7, 1, argv3));
+			Assert::AreEqual(false, checkNumberRange(7, 1, argv4));
+		}
+
+		TEST_METHOD(TestCheckRepetition)
+		{
+			char * argv1[] = { "Anything", "2", "3", "4", "5", "6", "7" };
+			char * argv2[] = { "Anything", "2", "2", "4", "5", "6", "7" };
+			Assert::AreEqual(true, checkRepetition(7, 1, argv1));
+			Assert::AreEqual(false, checkRepetition(7, 1, argv2));
+		}
+	};
+}
